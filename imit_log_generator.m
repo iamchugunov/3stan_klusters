75a28ada50405bf413054e2d64c57750ec9762e3
Alexander Chugunov <San4es_95@mail.ru>
2020-12-07 21:07:00 +0300

    Update imit_log_generator.m

--- imit_log_generator.m
+++ imit_log_generator.m
@@ -3,7 +3,7 @@ close all
 
 config = Config();
 
-pattern_lengths = [10 5 8 13 20];
+pattern_lengths = [6 8 10]; % такая запись значит что будет 3 разных паттерна соответственно длиной 6 8 и 10
 [pattern] = make_pattern(config, pattern_lengths);
 
 [imp, pattern_info] = make_dataset(config, pattern, 10000);
